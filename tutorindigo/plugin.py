d8dd7cf4d634da40ab7f649b67904849fcec0341
Ahmed Khalid <106074266+ahmed-arb@users.noreply.github.com>
2025-11-12 18:37:20 +0500

    feat: update npm package version to latest for footer component

--- tutorindigo/plugin.py
+++ tutorindigo/plugin.py
@@ -121,7 +121,7 @@ for mfe in indigo_styled_mfes:
             (
                 f"mfe-dockerfile-post-npm-install-{mfe}",
                 """
-RUN npm install @edly-io/wikimedia-frontend-component-footer@^3.0.0
+RUN npm install @edly-io/wikimedia-frontend-component-footer@latest
 RUN npm install '@edx/brand@npm:@edly-io/indigo-brand-openedx@^2.2.2'
 
 """,
